460272146c14695793249e218be276e1e15aa22b
ParkMimic <mimics626@gmail.com>
2026-03-11 22:58:09 +0900

    20260311_Park
    뱀서라이크 모작 3일차

---
+++ Assets/Undead Survivor/Codes/PoolManager.cs
@@ -0,0 +1,45 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    // .. 프리팹들을 보관할 변수
+    public GameObject[] prefabs;
+
+    // .. 풀 담당을 하는 리스트
+    List<GameObject>[] pools;
+
+    private void Awake()
+    {
+        pools = new List<GameObject>[prefabs.Length];
+
+        for (int index = 0; index < pools.Length; index++)
+        {
+            pools[index] = new List<GameObject>();
+        }
+    }
+
+    public GameObject Get(int index)
+    {
+        GameObject select = null;
+
+        foreach (GameObject item in pools[index])
+        {
+            if (!item.activeSelf)
+            {
+                select = item;
+                select.SetActive(true);
+                break;
+            }
+        }
+
+        if (!select)
+        {
+            select = Instantiate(prefabs[index], transform);
+            pools[index].Add(select);
+        }
+
+        return select;
+    }
+}
