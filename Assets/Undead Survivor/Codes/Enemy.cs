c43adf7c7824b33bbab3940492f5a950dba282a3
ParkMimic <mimics626@gmail.com>
2026-03-18 01:00:40 +0900

    20260318_Park
    뱀서 무기 만들기 및 프리팹 활용

--- Assets/Undead Survivor/Codes/Enemy.cs
+++ Assets/Undead Survivor/Codes/Enemy.cs
@@ -51,4 +51,26 @@ public class Enemy : MonoBehaviour
         maxHealth = data.health;
         health = data.health;
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Bullet"))
+            return;
+
+        health -= collision.GetComponent<Bullet>().damage;
+
+        if (health > 0)
+        {
+            // ..ive
+        }
+        else
+        {
+            Dead();
+        }
+    }
+
+    void Dead()
+    {
+        gameObject.SetActive(false);
+    }
 }
